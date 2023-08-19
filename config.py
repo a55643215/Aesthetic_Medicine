0c4ae4635931912aa16fd5b7b7c98658478e8106
a55643215 <138569500+a55643215@users.noreply.github.com>
2023-08-19 10:43:34 +0800

    up

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://medicine_offer:Vt6RqxnBcXH45JzKvz8wUqVJZi6Vq4Ny@dpg-cjg2jec1ja0c739odo7g-a.singapore-postgres.render.com/medicine_offer'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://medicine_offer:Vt6RqxnBcXH45JzKvz8wUqVJZi6Vq4Ny@dpg-cjg2jec1ja0c739odo7g-a.singapore-postgres.render.com/medicine_offer'
 
 class ProdConfig(Config):
     DEBUG = False
