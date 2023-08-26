b2666dad1f3ba0ea9c2934ae701756c2be729e56
a55643215 <138569500+a55643215@users.noreply.github.com>
2023-08-26 21:24:14 +0800

    up

--- events/admin.py
+++ events/admin.py
@@ -17,12 +17,12 @@ def list_reservation_event(event):
         print(reservation.is_canceled)
         user_set = reservation.user_id
         user=User.query.filter(User.id == user_set).first()
-        print(user.display_name)
-#         reservation_data_text += f'''預約日期: {reservation.booking_datetime}
-# 預約服務: {reservation.booking_service}
-# 姓名: {reservation.user_id.user.display_name}\n'''
+        
+        reservation_data_text += f'''預約日期: {reservation.booking_datetime}
+預約服務: {reservation.booking_service}
+姓名: {user.display_name}\n'''
 
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=reservation_data_text)
-    # )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reservation_data_text)
+    )
